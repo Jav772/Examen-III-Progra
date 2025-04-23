43ed1264a4ad233657ce26149f6515d9ddd06cf9
Jav772 <pipeherreracr@gmail.com>
2025-04-22 21:05:27 -0600

    Add files via upload

---
+++ Plantas.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE Plantas
+GO
+ 
+USE Plantas
+GO
+ 
+ 
+ create table CaracteristicasPlantas(
+PlantaID int identity primary key,
+Nombre varchar(50) not null,
+Hojas varchar (50) not null,
+Color varchar (50) not null
+)
+
+create table TipoPlantas(
+TipoID int identity primary key,
+Nombre varchar (50) not null,
+Tipo varchar (50) not null,
+PlantaID int foreign key references CaracteristicasPlantas(PlantaID),
+)
+ 
